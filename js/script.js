8a0083a20bc7ecbf628c3bf8cd68b34e45108e75
alicj <zichen.jiang@yahoo.com>
2017-11-20 02:03:39 -0500

    change id names to make them more meaningful

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var prevState = "";
-var curState = "1a";
+var curState = "begin-1a";
 var accountNumber = "65342";
 var passcode = "1234";
 var errorTime = 0;
@@ -7,7 +7,7 @@ var errorTime = 0;
 function nextState(s, time=400){
 	$("#"+curState).hide();
 
-	if (s == "3a") {
+	if (s == "error-3a") {
 		$(".error-time").html(3 - errorTime);
 		// if errorTime
 	}
@@ -25,76 +25,76 @@ function nextState(s, time=400){
 }
 
 $(document).ready(function() {
-	$("#1a").show();
+	$("#begin-1a").show();
 });
 
 $(document).on('click', '#homeBtn', function(event) {
 	event.preventDefault();
 	// TODO: need to eject card at certain state
-	nextState("1a");
+	nextState("begin-1a");
 });
 
 $(document).on('click', '#backBtn', function(event) {
 	event.preventDefault();
 	// TODO: need to eject card at certain state
-	if (curState == "2c" || curState == "4a") { prevState = "2a"}
+	if (curState == "account-number-2c" || curState == "passcode-4a") { prevState = "signin-2a"}
 	nextState(prevState);
 });
 
-$(document).on('click', '#1a', function(event) {
+$(document).on('click', '#begin-1a', function(event) {
 	console.log(event);
 	console.log(event.target.tagName)
 	if ($.inArray(event.target.tagName, ["SELECT", "OPTION"]) > -1) {return};
-	nextState("2a", 0);
+	nextState("signin-2a", 0);
 	$("#homeBtn").show();
 });
 
-$(document).on('click', '#insert-card', function(event) {
-	nextState("2b", 0);
+$(document).on('click', '.insert-card', function(event) {
+	nextState("insert-2b", 0);
 	$("#backBtn").show();
 });
 
-$(document).on('click', '#enter-account-number', function(event) {
-	nextState("2c", 0);
+$(document).on('click', '.enter-account-number', function(event) {
+	nextState("account-number-2c", 0);
 	$("#backBtn").show();
 });
 
-$(document).on('click', '#swipe-card', function(event) {
-	nextState("2d", 0);
+$(document).on('click', '.swipe-card', function(event) {
+	nextState("swipe-2d", 0);
 	$("#backBtn").show();
 });
 
-$(document).on('click', '.input-password', function(event) {
+$(document).on('click', '.input-passcode', function(event) {
 	event.preventDefault();
-	if (curState == "2c"){ // entered account number
+	if (curState == "account-number-2c"){ // entered account number
 		if($("#account-number").val() != accountNumber){
 			errorTime += 1
 			if (errorTime >= 3) {
-				nextState("3b");
+				nextState("max-error-3b");
 				$("#backBtn").hide();
 				return;
 			}
-			nextState("3a");
+			nextState("error-3a");
 			return;
 		}
 	}
 
-	nextState("4a");
+	nextState("passcode-4a");
 });
 
 $(document).on('click', '.log-in', function(event) {
 	event.preventDefault();
 	console.log($("#passcode").val())
 	if ($("#passcode").val() == passcode){
-		nextState("5a");
+		nextState("main-5a");
 	}else{
 		errorTime += 1;
 		if (errorTime >= 3) {
-			nextState("3b");
+			nextState("max-error-3b");
 			$("#backBtn").hide();
 			return;
 		}
-		nextState("3a")
+		nextState("error-3a")
 	}
 });
 
