881cc43dea4850a06e8a86cff54676e86deebcdb
alicj <zichen.jiang@yahoo.com>
2017-11-20 12:30:26 -0500

    add sign out and check balance screen

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var prevState = "";
-var curState = "begin-1a";
+// var curState = "begin-1a";
+var curState = "main-5a";
 var accountNumber = "65342";
 var passcode = "1234";
 var errorTime = 0;
@@ -15,30 +16,65 @@ function nextState(s, time=400){
 	if (time > 0) {
 		$("#wait").show();
 	}
+
+	//hide buttons
+	switch (curState) {
+		case "account-balance-15a":
+			$("footer .homeBtn").hide();
+			break;
+		case "trans-history-16a":
+			$(".historyBtn").hide();
+			break;
+		case "sign-out":
+			$("footer .homeBtn").hide();
+			break;
+	}
+
 	setTimeout(function(){
 		$("#wait").hide();
 		$("#"+s).show();
 		prevState = curState;
 		curState = s;
+		//show buttons
+		switch (curState) {
+			case "insert-2b":
+			case "account-number-2c":
+			case "swipe-2d":
+				$(".backBtn").show();
+				break;
+			case "account-balance-15a":
+				$("footer .homeBtn").hide();
+				$(".historyBtn").show();
+				break;
+			case "trans-history-16a":
+				$(".historyBtn").hide();
+				$(".backBtn").show();
+				$(".homeBtn").show();
+				break;
+			case "sign-out":
+				$("#sign-out-17a .homeBtn").show();
+				$("footer .homeBtn").hide();
+				break;
+		}
 	}, time);
 
 }
 
 $(document).ready(function() {
-	$("#begin-1a").show();
+	$("#"+curState).show();
 });
 
-$(document).on('click', '#homeBtn', function(event) {
+$(document).on('click', '.homeBtn', function(event) {
 	event.preventDefault();
 	// TODO: need to eject card at certain state
 	nextState("begin-1a");
 });
 
-$(document).on('click', '#backBtn', function(event) {
+$(document).on('click', '.backBtn', function(event) {
 	event.preventDefault();
 	// TODO: need to eject card at certain state
 	if (curState == "account-number-2c" || curState == "passcode-4a") { prevState = "signin-2a"}
-	nextState(prevState);
+		nextState(prevState);
 });
 
 $(document).on('click', '#begin-1a', function(event) {
@@ -46,32 +82,30 @@ $(document).on('click', '#begin-1a', function(event) {
 	console.log(event.target.tagName)
 	if ($.inArray(event.target.tagName, ["SELECT", "OPTION"]) > -1) {return};
 	nextState("signin-2a", 0);
-	$("#homeBtn").show();
+	$(".homeBtn").show();
 });
 
 $(document).on('click', '.insert-card', function(event) {
 	nextState("insert-2b", 0);
-	$("#backBtn").show();
 });
 
 $(document).on('click', '.enter-account-number', function(event) {
 	nextState("account-number-2c", 0);
-	$("#backBtn").show();
 });
 
 $(document).on('click', '.swipe-card', function(event) {
 	nextState("swipe-2d", 0);
-	$("#backBtn").show();
 });
 
 $(document).on('click', '.input-passcode', function(event) {
 	event.preventDefault();
 	if (curState == "account-number-2c"){ // entered account number
 		if($("#account-number").val() != accountNumber){
+			$("#account-number").val("")
 			errorTime += 1
 			if (errorTime >= 3) {
 				nextState("max-error-3b");
-				$("#backBtn").hide();
+				$(".backBtn").hide();
 				return;
 			}
 			nextState("error-3a");
@@ -88,13 +122,35 @@ $(document).on('click', '.log-in', function(event) {
 	if ($("#passcode").val() == passcode){
 		nextState("main-5a");
 	}else{
+		$("#passcode").val("")
 		errorTime += 1;
 		if (errorTime >= 3) {
 			nextState("max-error-3b");
-			$("#backBtn").hide();
+			$(".backBtn").hide();
 			return;
 		}
 		nextState("error-3a")
 	}
 });
 
+$(document).on('click', '.check-balance', function(event) {
+	event.preventDefault();
+	nextState("account-balance-15a");
+	// $("footer .homeBtn").hide();
+	// $(".historyBtn").show();
+});
+
+$(document).on('click', '.historyBtn', function(event) {
+	event.preventDefault();
+	nextState("trans-history-16a");
+	$(".historyBtn").hide();
+	// $(".backBtn").show();
+	// $(".homeBtn").show();
+});
+
+$(document).on('click', '.sign-out', function(event) {
+	event.preventDefault();
+	nextState("sign-out-17a");
+	// $("#sign-out-17a .homeBtn").show();
+	// $("footer .homeBtn").hide();
+});
